6cd65402bf164d893dd0ec958fa92995559c8d74
deputy <millotpg@mail.uc.edu>
2018-05-23 15:58:00 -0400

    Deputy gets invalid certificate. need to figure out why cert doesn't work

--- sheriff/certify.py
+++ sheriff/certify.py
@@ -1,10 +1,11 @@
 from subprocess import call
 import datetime
+import os
 
 # TODO: implement config file that determines how long the interval is
 # and what users get what permissions
 
 #def create_certificate(membership, username):
 def create_certificate(username):
-    call(['ssh-keygen', '-s', '../keys/host', '-I', username, '-n',
-        "root", '-V', '-1m:+30m', './tmp.pub'])
+    call(['ssh-keygen', '-s', '/opt/Sheriff/CA_keys/users_ca', '-I', username, '-n',
+        "root", '-V', '-1m:+30m', '/tmp/Sheriff_Public_Keys/temp.pub'])
--- sheriff/main.py
+++ sheriff/main.py
@@ -1,19 +1,28 @@
 #! /usr/bin/env python3
 from auth import auth_user
 import certify
-import os
+import os, errno
+import getpass
 
 def main():
     username = input()
     password = input()
     publicKey = input()
-    print(os.getcwd())
+    if not os.path.exists('/tmp/Sheriff_Public_Keys'):
+        try:
+            os.makedirs('/tmp/Sheriff_Public_Keys')
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+    #TODO Error handling!
     if(auth_user(username, password)):
-        with open('/home/sheriff/Desktop/Sheriff/sheriff/tmp.pub', 'w') as fl:
-            fl.write(str(publicKey))
+        with open('/tmp/Sheriff_Public_Keys/temp.pub', 'w') as fl:
+            fl.write(publicKey[2:-1])# get rid of the singlequotes and b on the endpoints
         certify.create_certificate(username)
+        with open('/tmp/Sheriff_Public_Keys/temp-cert.pub', 'r') as cert:
+            print(cert.read())
+
     else:
         print("Invalid username or password")
-
 
 main()
