c3e437971378ab208cf830bb31b1fef78b710486
deputy <millotpg@mail.uc.edu>
2018-06-04 09:27:18 -0400

    condensed and removed dependencies

--- deputy/main.py
+++ deputy/main.py
@@ -1,5 +1,4 @@
 #! ../env/bin/python3
-from create_keys import create_keys
 from subprocess import PIPE
 import subprocess
 import getpass
@@ -19,25 +18,33 @@ certificate files are written to:
 respectively.
 '''
 
+def create_keys(username):
+    subprocess.Popen(['./create_keys.sh', username]).wait()
+    with open(username + "_id_rsa", 'r') as priv_key_file:
+        priv_key = priv_key_file.read()
+
+    with open(username + "_id_rsa.pub") as pub_key_file:
+        pub_key = pub_key_file.read()
+
+    return pub_key
+
 def main():
     username = input('Username: ')
     password = getpass.getpass()
-    private_key, public_key = create_keys()
+    public_key = create_keys(username)
     args = ['ssh', config.CLIENT_USERNAME + '@192.168.184.157', '-p', '12345']
 
     with subprocess.Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True) as proc:
         proc.stdin.write(username + '\n')
         proc.stdin.write(password + '\n')
-        proc.stdin.write(public_key.decode('utf-8')+'\n')
+        proc.stdin.write(public_key +'\n')
         proc.stdin.flush()
         sheriff_responses = proc.stdout.readlines()
 
-    with open(username + '_id_rsa', 'wb') as private_key_file:
-        private_key_file.write(private_key)
-
-    with open(username + '_id_rsa.pub', 'wb') as public_key_file:
-        public_key_file.write(public_key)
-
+    if(sheriff_responses[-1] == 'False'):
+        print('Invalid username/password: Certificate could not be created.')
+        return
+        
     with open(username + '_id_rsa-cert.pub', 'w') as cert_file:
         cert_file.write(sheriff_responses[-1])
 
